75d93222ba540b22394110c22e93aa051df31e9c
Jordan Last <jordan.michael.last@gmail.com>
2015-12-26 15:03:49 -0700

    setting up webpack with TypeScript

--- www/components/app/AppComponent.ts
+++ www/components/app/AppComponent.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector: 'asl-app',
-	url: 'components/app/app.html'
+	templateUrl: 'components/app/app.html'
 })
 export class AppComponent {
 	
